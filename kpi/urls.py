7c48966d4c025a15a0e3fdecb50eb9e0933e65a0
Alexandr Volynkin <sv200330@mail.ru>
2022-07-24 22:38:00 +0300

    Rename requirements

--- kpi/urls.py
+++ kpi/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'kpi'
 
 urlpatterns = [
-    path('<int:pk>/', views.home, name='home'),
+    path('fill/<int:pk>/', views.home, name='home'),
     path('answers/<int:pk>/', views.home_2, name='home_2'),
-
 ]
